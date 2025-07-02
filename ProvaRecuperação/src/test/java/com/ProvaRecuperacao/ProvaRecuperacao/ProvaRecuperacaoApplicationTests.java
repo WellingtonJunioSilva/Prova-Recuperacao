95d405d4efe72954c6af01b12d7b6316f8a20bb0
Wellington Junio <Wjuniossantos009@gmail.com>
2025-07-02 19:22:05 -0300

    Primeiros commits

---
+++ ProvaRecuperação/src/test/java/com/ProvaRecuperacao/ProvaRecuperacao/ProvaRecuperacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProvaRecuperacao.ProvaRecuperacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaRecuperacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
